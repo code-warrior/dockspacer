b4178cd725e24b51b358c8682e3014c5aff67f8b
Roy Vanegas <roy@thecodeeducators.com>
2014-08-17 08:06:09 -0400

    Start adding comments to “main.c”

--- main.c
+++ main.c
@@ -18,15 +18,19 @@ int main( int argc, char **argv )
 
    printf( "\n\nThis program creates empty items in your Dock that you can use as spacers. After you enter the amount of spacers you want, the Dock will flash and you will have your new spacers near the Recycle Bin. You can then move them as you would any other Dock item.\n\n%s%s Note %s This program does not remove any Dock items. You’ll need to remove unwanted Dock spacers manually.\n\nYou may cancel this program with the keyboard sequence CNTRL + C, or enter the number 0.\n\nHow many spacers would you like? ", RED, BGYELLOW, NORMAL );
 
+   // Get user input
    fgets( input, sizeof( input ), stdin );
 
+   // Replace the newline char with the null character
    input[ strlen( input ) ] = '\0';
 
+   // If user input contains only one character (the null character), then nothing was entered at the prompt. Exit.
    if( strlen( input ) == 1 ) {
       printf( "%s%s Empty entry. %s%s Please run this program again... %s\n", RED, BGYELLOW, NORMAL, RED, NORMAL );
       exit( -1 );
    }
 
+   // Copy the user input
    copy_of_input = malloc( strlen( input ) * sizeof( char ) );
    strncpy( copy_of_input, trim( input ), strlen( input ) );
 
