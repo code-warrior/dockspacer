8fee067f2c5afe9431387659009d77ec5461694c
Roy A Vanegas <roy@thecodeeducators.com>
2015-11-01 12:42:04 -0500

    Fit comments within an 85-character container

--- libs/stoi.c
+++ libs/stoi.c
@@ -5,9 +5,9 @@
 #include "../constants.h"
 #include "stoi.h"
 
-/* =========={ STOI - STRING TO INTEGER }=============================
+/* =========={ STOI - STRING TO INTEGER }========================================
    Take an array of characters as a number and return its integer equivalent.
-   ================================================================ */
+   ============================================================================== */
 int stoi( char string[] )
 {
    int length = -1;
@@ -16,20 +16,21 @@ int stoi( char string[] )
    while( string[ ++length ] != '\0' )
       ;
 
-   /* ================================================================
-      Check that each character in the string has a valid integer equivalent. If it
-      does, move on to the next character. If it does not, return -1 (INVALID_INPUT).
-      ============================================================= */
+   /* ============================================================================
+      Check that each character in the string has a valid integer equivalent. If
+      it does, move on to the next character. If it does not, return -1
+      (INVALID_INPUT).
+      ============================================================================ */
    for( index = 0; index < length - 1; index++ )
       if( ( isdigit( string[ index ] ) ) != 0 )
          continue;
       else
          return INVALID_INPUT;
 
-   /* ================================================================
-      If program control reaches this point, a valid number in the form of a string
-      was fed to this function. Thus, it is safe to convert the string to an integer,
-      then return that conversion.
-      ============================================================= */
+   /* ============================================================================
+      If program control reaches this point, a valid number in the form of a
+      string was fed to this function. Thus, it is safe to convert the string to
+      an integer, then return the conversion.
+      ============================================================================ */
    return ( int ) strtol( string, (char**) NULL, 10 );
 }
